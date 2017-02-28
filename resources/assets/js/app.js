af31ae52d796ff4b9de09f2c6e188a5cf1b46b2b
Roger Melich <rogermelich@gmail.com>
2017-02-28 21:34:30 +0100

    Implements Chat Form and ChatMessages into Vue.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,7 +14,33 @@ require('./bootstrap');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('chat-messages', require('./components/ChatMessages.vue'));
+Vue.component('chat-form', require('./components/ChatForm.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+
+    data: {
+        messages: []
+    },
+
+    created() {
+        this.fetchMessages();
+    },
+
+    methods: {
+        fetchMessages() {
+            axios.get('/messages').then(response => {
+                this.messages = response.data;
+            });
+        },
+
+        addMessage(message) {
+            this.messages.push(message);
+
+            axios.post('/chat', message).then(response => {
+                console.log(response.data);
+            });
+        }
+    }
 });
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -12,7 +12,7 @@
 
                 <div class="box box-primary direct-chat direct-chat-primary">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Direct Chat</h3>
+                        <h3 class="box-title">Chat</h3>
 
                         <div class="box-tools pull-right">
                             <span data-toggle="tooltip" title="3 New Messages" class="badge bg-light-blue">3</span>
@@ -28,42 +28,7 @@
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
-                        <!-- Conversations are loaded here -->
-                        <div class="direct-chat-messages">
-                            <!-- Message. Default to the left -->
-                            <div class="direct-chat-msg">
-                                <div class="direct-chat-info clearfix">
-                                    <span class="direct-chat-name pull-left">Alexander Pierce</span>
-                                    <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
-                                </div>
-                                <!-- /.direct-chat-info -->
-                                <img class="direct-chat-img" src="./img/user1-128x128.jpg" alt="Message User Image">
-                                <!-- /.direct-chat-img -->
-                                <div class="direct-chat-text">
-                                    Is this template really for free? That's unbelievable!
-                                </div>
-                                <!-- /.direct-chat-text -->
-                            </div>
-                            <!-- /.direct-chat-msg -->
-
-                            <!-- Message to the right -->
-                            <div class="direct-chat-msg right">
-                                <div class="direct-chat-info clearfix">
-                                    <span class="direct-chat-name pull-right">Sarah Bullock</span>
-                                    <span class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
-                                </div>
-                                <!-- /.direct-chat-info -->
-                                <img class="direct-chat-img" src="./img/user3-128x128.jpg" alt="Message User Image">
-                                <!-- /.direct-chat-img -->
-                                <div class="direct-chat-text">
-                                    You better believe it!
-                                </div>
-                                <!-- /.direct-chat-text -->
-                            </div>
-                            <!-- /.direct-chat-msg -->
-                        </div>
-                        <!--/.direct-chat-messages-->
-
+                        <chat-messages v-bind:messages="messages"></chat-messages>
                         <!-- Contacts are loaded here -->
                         <div class="direct-chat-contacts">
                             <ul class="contacts-list">
@@ -89,15 +54,16 @@
                     </div>
                     <!-- /.box-body -->
                     <div class="box-footer">
-                        <form action="#" method="post">
-                            {{ csrf_field() }}
-                            <div class="input-group">
-                                <input type="text" name="message" placeholder="Type Message ..." class="form-control">
-                                <span class="input-group-btn">
-                        <button type="submit" class="btn btn-primary btn-flat">Send</button>
-                      </span>
-                            </div>
-                        </form>
+                        {{--<form action="#" method="post">--}}
+                            {{--{{ csrf_field() }}--}}
+                            {{--<div class="input-group">--}}
+                                {{--<input type="text" name="message" placeholder="Type Message ..." class="form-control">--}}
+                                {{--<span class="input-group-btn">--}}
+                        {{--<button type="submit" class="btn btn-primary btn-flat">Send</button>--}}
+                      {{--</span>--}}
+                            {{--</div>--}}
+                        {{--</form>--}}
+                        <chat-form :user=" {{ Auth::user() }}" v-on:messagesent="addMessage"></chat-form>
                     </div>
                     <!-- /.box-footer-->
                 </div>
