d60ea76437982842f919ee0f373d8cac949ce3fb
Roger Melich <rogermelich@gmail.com>
2017-03-21 21:33:13 +0100

    Add Echo socket.io

--- client/client.js
+++ client/client.js
@@ -1,3 +1,11 @@
-/**
- * Created by roger on 21/03/17.
- */
+var socket = require('socket.io-client')('http://localhost:3000');
+socket.on('connect', function(){
+    console.log('Client connected ok to localhost port 3000')
+});
+socket.on('chat:App\\Events\\MessageSent', function(data){
+    console.log('Event chat receive')
+    console.log(data)
+});
+socket.on('disconnect', function(){
+    console.log('Client disconnected ok from localhost port 3000')
+});
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -50,9 +50,15 @@ Vue.component('login-input-field', require('./components/LoginInputField.vue'));
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from "laravel-echo"
+import Echo from "laravel-echo"
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
 //     key: 'your-pusher-key'
 // });
+
+
+window.Echo = new Echo({
+    broadcaster: 'socket.io',
+    host: 'http://localhost:3000'
+});
