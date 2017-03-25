35811e147ff876ac58c618a596fc3bb99638e6fe
Roger Melich <rogermelich@gmail.com>
2017-03-25 19:55:59 +0100

    Add event chat

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace RogerMelich\Chat\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use RogerMelich\Chat\Message;
+use RogerMelich\Chat\User;
+
+class MessageSent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+
+    public$message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, Message $message)
+    {
+        $this->user = $user;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('chat');
+    }
+}
